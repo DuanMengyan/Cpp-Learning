37b72bb3f91f0935c6ee5fb9de0f2a4424557450
DuanMengyan <dmy_email@163.com>
2018-06-22 22:27:44 +0800

    Day01.cpp

--- SaoDiSeng视频/Day01.cpp
+++ SaoDiSeng视频/Day01.cpp
@@ -1,12 +1,16 @@
+﻿/*************************************************************************
+> File Name: C++基础Day01.cpp
+> Author: DMY
+> Mail: dmy_email@163.com
+> Created Time: 2018年06月22日 星期五
+> Topic:面向对象基础及易犯错误
+************************************************************************/
+
 
 #include <iostream>
-
-
-
 using namespace std;
 
 
-//C++基础Day01
 //面向过程的方法求圆的面积
 void S_Circle_01()
 {
@@ -20,12 +24,17 @@ void S_Circle_01()
 }
 
 
-class MyCircle
+//面向对象的方法求圆的面积
+//1.类的抽象 定义成员变量和成员函数
+//2.实例化 用类定义变量(对象)
+//3.面向过程加工的是一个一个函数；面向过程加工的是一个一个的类；
+
+class MyCircle1
 {
 public:
-	MyCircle();
-	~MyCircle();
-public:
+	double m_r;
+	double m_s;
+
 	void setR(double r)
 	{
 		m_r = r;
@@ -39,28 +48,13 @@ public:
 		m_s = 3.14*m_r*m_r;
 		return m_s;
 	}
-
-private:
-	double m_r;
-	double m_s;
 };
 
-MyCircle::MyCircle()
-{
-}
 
-MyCircle::~MyCircle()
-{
-}
-
-//面向对象的方法求圆的面积
-//1.类的抽象 定义成员变量和成员函数
-//2.实例化 用类定义变量(对象)
-//3.面向过程加工的是一个一个函数；面向过程加工的是一个一个的类；
-
+//面向对象求解圆的面积
 void S_Circle_02()
 {
-	MyCircle c1, c2;
+	MyCircle1 c1;
 	double r;
 	std::cout << "请输入圆c1的半径：";
 	std::cin >> r;
@@ -70,13 +64,50 @@ void S_Circle_02()
 }
 
 
+//类中不定义成员函数易犯错误
+class MyCircle2
+{
+public:
+	double m_r;
+	double m_s = 3.14*m_r*m_r;
+
+	void setR(double r)
+	{
+		m_r = r;
+	}
+	double getR()
+	{
+		return m_r;
+	}
+	double getS()
+	{
+		m_s = 3.14*m_r*m_r;
+		return m_s;
+	}
+};
+
+
+//类中定义成员函数的重要性，因为成员变量的值计算过程只在初始化的时候执行
+void S_Circle_03()
+{
+	MyCircle2 c2;
+	double r;
+	std::cout << "请输入圆c2的半径：";
+	std::cin >> r;
+	std::cout << "圆c1的半径为：" << c2.getR() << std::endl;//此时c2的半径m_r为实例化对象c1时的随机值（乱码）；
+	c2.setR(r);
+	std::cout << "圆c1的半径为：" << c2.getR() << std::endl;//此时c2的半径m_r为用户输入的值；
+	std::cout << "圆c1的面积m_s为：" << c2.m_s << std::endl;//此时c2的面积m_s为实例化对象c1时的随机值（乱码）；
+	std::cout << "圆c1的面积m_s为：" << c2.getS() << std::endl;//此时c2的面积m_s为计算得到的面积；
+	std::cout << "圆c1的面积m_s为：" << c2.m_s << std::endl;//此时c2的面积m_s为计算得到的面积；
+}
+
 
 
 int main()
 {
-
-
-	S_Circle_02();
+	//S_Circle_01();
+	//S_Circle_02();
+	S_Circle_03();
 	return 0;
-
 }
