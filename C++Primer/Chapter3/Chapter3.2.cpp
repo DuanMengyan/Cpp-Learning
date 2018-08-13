0ee0b4d1d089c56fd22af4c0a8df50e3ddafb42f
DuanMengyan <dmy_email@163.com>
2018-08-13 19:12:24 +0800

    edit

--- C++Primer/Chapter3/Chapter3.2.cpp
+++ C++Primer/Chapter3/Chapter3.2.cpp
@@ -6,7 +6,7 @@
 > Author: DMY
 > Mail: dmy_email@163.com
 > Created Time: 2018年08月7日 星期二
-> Last edited time: 2018年8月11日 星期六
+> Last edited time: 2018年8月13日 星期一
 > Topic:C++Primer Chapter3.2标准库类型string
 ************************************************************************/
 
@@ -140,6 +140,72 @@ void Fun_chars_in_string_operate()
 
 
 	//使用下标执行随机访问
+	//const string hexdigits = "0123456789ABCDEF";
+	//string result;
+	//string::size_type n;
+	//while (cin>>n)
+	//{
+	//	if (n < hexdigits.size())		//n小于16
+	//		result += hexdigits[n];		
+	//}
+	//cout << result<<endl;
+
+	//ex3.6
+	//string str = "hello world";
+	//cout << str << endl;
+	//for (auto &c : str)
+	//{
+	//	c = 'X';
+	//}
+	//cout << str << endl;
+
+	//ex3.7
+	//string str = "hello world";
+	//cout << str << endl;
+	//for (auto c : str)		//c是字符类型，而不是字符引用，不能改变原有字符串中的字符
+	//{
+	//	c = 'X';
+	//}
+	//cout << str << endl;
+
+	//ex3.8
+	//string str = "hello world";
+	//cout << str << endl;
+	//decltype(str.size()) index = 0;
+	//while (index!=str.size())
+	//{
+	//	str[index++] = 'X';
+	//}
+	//cout << str << endl;
+	
+	//for (decltype(str.size()) i = 0; i < str.size(); ++i)
+	//{
+	//	str[i] = 'X';
+	//}
+	//cout << str << endl;
+
+	//ex3.9  //无效
+	//string s;
+	//cout << s[0] << endl;
+
+	//ex3.10
+	//string str = "hello,w.orld!",result;
+	//for (auto i:str)
+	//{
+	//	if (!ispunct(i))
+	//		result += i;
+	//}
+	//cout << result << endl;
+
+	//ex3.11
+	const  string s = "Keep out!";		
+	for (auto &c : s)	              //c是一个常量字符引用,否则，就可以通过c改变字符串的值
+	{
+		cout << c;			//合法
+		//c = 'X';			//不合法	
+	}
+		
+
 }
 
 int main()
