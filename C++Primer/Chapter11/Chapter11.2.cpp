16b77752978ad02751142725ed089d20867deaae
DuanMengyan <39231834+DuanMengyan@users.noreply.github.com>
2019-01-03 23:05:04 +0800

    Chapter11.2

--- C++Primer/Chapter11/Chapter11.2.cpp
+++ C++Primer/Chapter11/Chapter11.2.cpp
@@ -19,30 +19,11 @@ using namespace std;
 
 using Families = map<string, vector<string>>;
 
-auto make_families()
-{
-	Families families;
-	for (string ln; cout << "Last name:\n", cin >> ln && ln != "@q";)
-		for (string cn; cout << "|-Children's names:\n", cin >> cn && cn != "@q";)
-			families[ln].push_back(cn);
-	return families;
-}
-
-auto print(Families const& families)
-{
-	for (auto const& family : families)
-	{
-		cout << family.first << ":\n";
-		for (auto const& child : family.second)
-			cout << child << " ";
-		cout << "\n";
-	}
-}
 
 
 //Chapter11.2 关联容器概述
 
-//11.1 定义关联容器
+//11.2.1 定义关联容器
 void Fun_Defining_an_Associative_Container()
 {
 	map<string, size_t> word_count;
@@ -74,11 +55,11 @@ void Fun_Defining_an_Associative_Container()
 	//ex11.7
 	//map<string, vector<string>> name_map;	
 	//vector<string> kids_name;
-	//cout << "Last name:" << endl;
-	//for(string first_name; cin >> first_name;cout<<"Last name:"<<endl)
+	//
+	//for(string first_name; cout << "Last name:" << endl,cin >> first_name;)
 	//{
-	//	cout << "First name:" << endl;
-	//	for (string name; cin >> name&&name != "end";cout<<"First name:"<<endl)
+	//	
+	//	for (string name; cout << "First name:" << endl,cin >> name&&name != "end";)
 	//	{
 
 	//		kids_name.push_back(name);
@@ -97,16 +78,16 @@ void Fun_Defining_an_Associative_Container()
 	//}
 
 	//ex11.8
-	vector<string> vec; 
-	string str;
-	while (cin>>str)
-	{
-		if ( find(vec.begin(),vec.end(),str) == vec.end())
-		{
-			vec.push_back(str);
-		}
-	}
-	printitem(vec);
+	//vector<string> vec; 
+	//string str;
+	//while (cin>>str)
+	//{
+	//	if ( find(vec.begin(),vec.end(),str) == vec.end())
+	//	{
+	//		vec.push_back(str);
+	//	}
+	//}
+	//printitem(vec);
 
 
 	//无论添加或者删除元素，set始终有序，且元素唯一
@@ -116,11 +97,42 @@ void Fun_Defining_an_Associative_Container()
 }
 
 
-//11.2 关键字类型的要求
+bool compareIsbn(const Sales_data &lhs,const Sales_data &rhs)
+{
+	return lhs.isbn() < rhs.isbn();
+}
+
+//11.2.2 关键字类型的要求
+void Fun_Requirements_on_Key_Type()
+{
+	//有序容器关键字类型必须定义元素比较的方法
+
+	//有序容器的关键字类型
+	//如果一个类型定义了“行为正常”的<运算符，则它可以用作关键字类型
+
+	//使用关键字类型的比较函数,提供一个指向指定函数的指针
+	//multiset<Sales_data, decltype(compareIsbn)*> bookstore(compareIsbn);
+
+	//ex11.9
+	//map<string, list<size_t>> words_map;
+
+	//ex11.10
+	map<vector<int>::iterator, int> vec_map;
+	map<list<int>::iterator, int> lst_map;
+
+	//vector可以，list不行，list<int>::iterator是双向迭代器，没有定义<比较运算符
+	vector<int> vi;
+	vec_map.insert(pair<vector<int>::iterator, int>(vi.begin(), 0));
 
 
+	//ex11.11
+	//函数指针
+	//using com = bool(*)(const Sales_data &lhs, const Sales_data &rhs);
+	//multiset<Sales_data, com>bookstore(compareIsbn);
+}
 
-//11.3 pair类型
+
+//11.2.3 pair类型
 
 
 
@@ -128,7 +140,9 @@ void Fun_Defining_an_Associative_Container()
 int main()
 {
 	//11.1 定义关联容器
-	Fun_Defining_an_Associative_Container();
+	//Fun_Defining_an_Associative_Container();
+	//11.2.2 关键字类型的要求
+	Fun_Requirements_on_Key_Type();
 	return 0;
 }
 
