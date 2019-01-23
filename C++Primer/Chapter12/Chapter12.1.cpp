2fcca184eea1577989547b75e883ab839e0e5401
DuanMengyan <39231834+DuanMengyan@users.noreply.github.com>
2019-01-23 14:35:35 +0800

    Chapter12.1.5

--- C++Primer/Chapter12/Chapter12.1.cpp
+++ C++Primer/Chapter12/Chapter12.1.cpp
@@ -7,7 +7,7 @@
 > Author: DMY
 > Mail: dmy_email@163.com
 > Created Time: 2019年1月10日 星期四
-> Last edited time: 2019年1月21日 星期一
+> Last edited time: 2019年1月22日 星期二
 > Topic:C++Primer Chapter12.1 动态内存与智能指针
 ************************************************************************/
 
@@ -337,15 +337,140 @@ void Fun_Using_shared_ptrs_with_new()
 
 
 	//ex12.13
-	auto sp = make_shared<int>();
-	auto p = sp.get();
-	delete p;					//默认初始化为0的动态内存被释放掉，sp变成一个空悬指针，报错
-	
+	//auto sp = make_shared<int>();
+	//auto p = sp.get();
+	//delete p;					//默认初始化为0的动态内存被释放掉，sp变成一个空悬指针，报错
 
-	//ex12.14
 }
 
+struct connection
+{
+	//表示使用连接所需的信息
+	string ip;
+	int port;
+	connection(string ip_, int port_) :ip(ip_), port(port_) {}
+};
 
+struct destination
+{
+	//表示正在连接什么
+	string ip;
+	int port;
+	destination(string ip_, int port_) :ip(ip_), port(port_) {}
+};
+
+connection connect(destination *pDest)
+{	//打开连接
+	shared_ptr<connection> pConn(new connection(pDest->ip, pDest->port));
+	cout << "creating connection...(" << pConn.use_count() << ")" << endl;
+	return *pConn;
+}
+
+void disconnect(connection pConn) 
+{
+	//关闭给定连接
+	cout << "connect close...(" << pConn.ip << " : " << pConn.port << ")" << endl;
+}
+
+void end_connection(connection *pConn)
+{
+	disconnect(*pConn);
+}
+
+void f(destination &d /* 其他参数 */)
+{
+	//获得一个连接，记住使用完以后要关闭它
+	connection c = connect(&d);
+	//shared_ptr<T> p(q, d) p接管了内置指针q所指向的对象的所有权，p将使用可调用对象d来代替delete
+	
+	shared_ptr<connection> p(&c, end_connection);
+	//shared_ptr<connection> p(&c, [](connection *a) {return disconnect(*a); });
+	cout << "connecting now...(" << p.use_count() << ")" << endl;
+	//使用连接
+	//退出前忘记调用disconnect，就无法关闭c了
+}
+
+//12.1.4 智能指针和异常
+void Fun_Smart_Pointers_and_Exceptions()
+{
+	//使用智能指针，正常处理结束或者发生异常导致函数退出，局部对象都会被销毁，内存会被释放
+	//使用内置指针管理内存，在new之后对应的delete之前发生异常，内存不会被释放
+
+	//智能指针和哑类（没有定义析构函数）
+	//使用我们自己的释放操作
+	//当一个share_ptr被销毁时，它默认地对它管理的指针进行delete操作，
+	//我们定义一个函数来代替delete ，这个删除器函数必须能够完成对shared_ptr中保存的指针进行释放的操作
+
+
+	//智能指针陷阱：使用智能指针坚持的基本规范
+	//不使用相同的内置指针值初始化（或reset）多个智能指针
+	//不delete get()返回的指针
+	//不使用get()初始化或reset另一个智能指针
+	//如果使用get()返回的指针，当最后一个对应的智能指针销毁后，指针就无效了
+	//如果使用智能指针管理的资源不是new分配的内存，记住传递给它一个删除器
+
+	//ex12.14
+	
+	//ex12.15
+	//destination dest("202.118.176.67", 3316);
+	//f(dest);
+}
+
+void f_uniqptr(destination &d)
+{
+	connection c = connect(&d);
+	unique_ptr<connection, decltype(end_connection)*> uni_p(&c, end_connection);
+	//使用连接
+	//当f_uniqptr退出时，即使是由于异常退出，connection会被正确关闭
+}
+
+//12.1.5 unique_ptr
+void Fun_unique_ptr()
+{
+	//某个时刻只能有一个unique_ptr指向一个给定对象
+	//定义unique_ptr时，需要将其绑定到一个new返回的指针上，初始化必须采用直接初始化
+	//unique_ptr不支持普通的拷贝或赋值操作
+	//unique_ptr<string> p1(new string("hello"));
+	//cout << *p1 << endl;						//
+	//unique_ptr<string> p2(p1.release());		//p1放弃对指针的控制权，返回指针，内存所有权转向p2，并将p1置为空
+	//cout << *p2 << endl;
+	//unique_ptr<string> p3(new string("Trex"));
+	//p2.reset(p3.release());						//reset释放了p2原来指向的内存，保存Trex的内存所有权从p3转向了p2
+	//cout << *p2 << endl;
+	////p3 = nullptr;		//释放p3所指向的对象，将p3置为空
+	////release返回的指针通常用来初始化另一个智能指针或给另一个智能指针赋值
+	////p2.release();		//p2不会释放内存，而且我们丢失了指针
+	//auto p = p2.release();	//一个普通指针保存release返回的指针，程序要负责资源的释放
+	//delete p;
+
+	//传递unique_ptr参数和返回unique_ptr
+	//我们可以拷贝或赋值一个将要被销毁的unique_ptr
+
+	//向unique_ptr传递删除器
+	//我们必须在尖括号中unique_ptr指向的类型之后提供删除器类型
+	//在创建或reset一个这种unique_ptr类型的对象时，必须提供一个指定类型的可调用对象（删除器）
+
+	//ex12.16
+	//unique_ptr<int> p1(new int(5));
+	//unique_ptr<int> p2;
+	////p2 = p1;
+	////p2.reset(p1.release());
+	//cout << *p2 << endl;
+	
+	//ex12.17
+	//int ix = 1024, *pi = &ix, *pi2 = new int(2048);
+	//typedef unique_ptr<int> IntP;
+	//IntP p0(ix);  //不能用int类型初始化unique_ptr<int>
+	//IntP p1(pi);	//可以通过编译，但是超出p1作用范围，p1被销毁，将调用delete释放该对象，但是1024并不是动态内存，会报错
+	//IntP p2(pi2);	//可以编译，但是会导致空悬指针（pi2）
+	//IntP p3(&ix);	//但是超出p3作用范围,会调用delete销毁一个不是动态内存的对象
+	//IntP P4(new int(2048));  //正确
+	//IntP p5(p2.get());		//会造成双重释放
+
+	//ex12.18
+	//因为unique_ptr“拥有”它所指向的对象，shared_ptr可以与其他智能指针共享对象，不需要释放
+
+}
 
 int main()
 {
@@ -354,8 +479,11 @@ int main()
 	//12.1.2 直接管理内存
 	//Fun_Managing_Memory_Directly();
 	//12.1.3 shared_ptr和new结合使用
-	Fun_Using_shared_ptrs_with_new();
-
+	//Fun_Using_shared_ptrs_with_new();
+	//12.1.4 智能指针和异常
+	//Fun_Smart_Pointers_and_Exceptions();
+	//12.1.5 unique_ptr
+	Fun_unique_ptr();
 
 	return 0;
 }
